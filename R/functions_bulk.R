69e04f9230ee8966c8997d29b67726137d5f78d1
stemangiola <mangiolastefano@gmail.com>
2019-12-06 08:43:14 +1100

    change tsne label
    Former-commit-id: 1a8b6a298d3ff4d4d4955ffc72a0524b355cfa1a
    Former-commit-id: 31b6cfd65120add375e6f540e49f9d139d6ebe81
    Former-commit-id: 8aead6e6d836ca73b38b7fc71caf7208d540203d

--- R/functions_bulk.R
+++ R/functions_bulk.R
@@ -1435,7 +1435,7 @@ get_reduced_dimensions_TSNE_bulk <-
 			do.call(Rtsne::Rtsne, c(list(df_tsne), arguments)) %$%
 			Y %>%
 			as_tibble(.name_repair = "minimal") %>%
-			setNames(c("tSNE 1", "tSNE 2")) %>%
+			setNames(c("tSNE1", "tSNE2")) %>%
 
 			# add element name
 			mutate(!!.element := df_tsne %>% rownames) %>%
