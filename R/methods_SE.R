31ca9b6084d04afa6172a77db78122a57184a4e0
stemangiola <mangiolastefano@gmail.com>
2023-03-03 14:31:53 +1100

    add ellipsis

--- R/methods_SE.R
+++ R/methods_SE.R
@@ -1106,7 +1106,6 @@ setMethod(
 																					 prefix = "",
 																					 ...)
 {
-
   # DEPRECATION OF .constrasts
   if (is_present(.contrasts) & !is.null(.contrasts)) {
 
@@ -1164,7 +1163,8 @@ such as batch effects (if applicable) in the formula.
 				test_above_log2_fold_change = test_above_log2_fold_change,
 				scaling_method = scaling_method,
 				omit_contrast_in_colnames = omit_contrast_in_colnames,
-				prefix = prefix
+				prefix = prefix,
+				...
 			),
 
 			# DESeq2
@@ -1175,7 +1175,8 @@ such as batch effects (if applicable) in the formula.
 				method = method,
 				scaling_method = scaling_method,
 				omit_contrast_in_colnames = omit_contrast_in_colnames,
-				prefix = prefix
+				prefix = prefix,
+				...
 			),
 
 			# Else error
