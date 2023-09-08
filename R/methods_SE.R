14848e83a9fd4e032a825fa513ebc4355b7077f7
stemangiola <mangiolastefano@gmail.com>
2023-09-08 19:46:38 +1000

    use Matrix library for sparse counts

--- R/methods_SE.R
+++ R/methods_SE.R
@@ -1584,6 +1584,7 @@ setMethod("keep_variable",
 #'
 #' @importFrom purrr map_chr
 #' @importFrom tidyr unite
+#' @importFrom Matrix colSums
 #'
 #' @docType methods
 #' @rdname keep_variable-methods
@@ -1711,7 +1712,7 @@ setMethod("keep_variable",
 			min.count = minimum_counts,
 			group = string_factor_of_interest,
 			min.prop = minimum_proportion,
-			lib.size = colSums(., na.rm=TRUE)
+			lib.size = Matrix::colSums(., na.rm=TRUE)
 		) %>%
 		not() %>%
 		which %>%
