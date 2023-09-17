a276b5a3755881c6501c578e98f74e2c6777b727
stemangiola <mangiolastefano@gmail.com>
2023-09-17 18:30:02 +1000

    add class detals

--- R/glmmSeq.R
+++ R/glmmSeq.R
@@ -407,6 +407,34 @@ glmerCore = function (geneList, fullFormula, reduced, data, control, offset,
 
 }
 
+# From glmmSeq
+
+# Class definitions
+
+setClassUnion("character_or_list", c("character", "list"))
+setClassUnion("df_or_matrix", c("data.frame", "matrix"))
+setClassUnion("list_or_matrix", c("list", "matrix"))
+setClassUnion("formulaOrNULL", c("formula", "NULL"))
+
+#' An S4 class to define the glmmSeq output
+#'
+#' @keywords internal
+#' @noRd
+#'
+#' @slot info List including the matched call, dispersions, offset, designMatrix
+#' @slot formula The model formula
+#' @slot stats Statistics from fitted models
+#' @slot predict Predicted values
+#' @slot reduced Optional reduced formula for LRT
+#' @slot countdata The input expression data with count data in rows
+#' @slot metadata The input metadata
+#' @slot modelData Model data for predictions
+#' @slot optInfo Information on whether the model was singular or converged
+#' @slot errors Any errors
+#' @slot vars List of variables stored from the original call, including the
+#'   `id` variable (by default automatically identified from the random effect
+#'   term in the model) and `removeSingles` argument
+
 setClass("GlmmSeq", slots = list(
   info = "list",
   formula = "formula",
