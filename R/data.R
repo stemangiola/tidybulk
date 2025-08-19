cb3b938d34767945dc42d62202ec6fb1c7cdcaee
Stefano Mangiola <mangiolastefano@gmail.com>
2025-08-19 21:46:46 +0930

    Update .Rbuildignore to exclude vignettes and add reference documentation in data.R
    - Added exclusion for the vignettes/egsea directory in .Rbuildignore to streamline build process.
    - Included a reference citation in data.R for the Cibersort reference, enhancing documentation clarity.

--- R/data.R
+++ R/data.R
@@ -1,4 +1,6 @@
 #' Cibersort reference
+#' 
+#' @references Newman, A. M., Liu, C. L., Green, M. R., Gentles, A. J., Feng, W., Xu, Y., Hoang, C. D., Diehn, M., & Alizadeh, A. A. (2015). Robust enumeration of cell subsets from tissue expression profiles. Nature Methods, 12(5), 453–457. https://doi.org/10.1038/nmeth.3337
 #'
 #'
 "X_cibersort"
